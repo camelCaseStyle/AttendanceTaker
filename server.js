95688530211870f13ae3abd7d3b95a4b24e3e19a
Anubhav <anubhav.ashish9@gmail.com>
2021-08-28 22:08:50 +1000

    rename index.js to server.js as that is what AWS looks for

--- server.js
+++ server.js
@@ -22,23 +22,6 @@ app.get("/getAllStudents", (req, res) =>{
     res.send(db.data)
 })
 
-app.post('/generateExcelFile', (req, res) =>{
-    // console.log(req.body.students)
-    // let wb = XLSX.utils.book_new();
-    // let ws = XLSX.utils.json_to_sheet([
-    //     { S:1, h:2, e:3, e_1:4, t:5, J:6, S_1:7 },
-    //     { S:2, h:3, e:4, e_1:5, t:6, J:7, S_1:8 }
-    //   ], {header:["S","h","e","e_1","t","J","S_1"]});
-    // XLSX.utils.book_append_sheet(wb, ws, 'attendance');
-    
-
-    // // XLSX.writeFile(wb,"Attendance.xlsx", {
-    // //     type: 'file',
-    // //     bookType: 'xlsx',
-    // //     sheet:'Attendance'
-    // // });
-    // res.send(XLSX.writeFile(wb, 'out.xlsx'));
-}); 
 function s2ab(s) {
     var buf = new ArrayBuffer(s.length);
     var view = new Uint8Array(buf);
